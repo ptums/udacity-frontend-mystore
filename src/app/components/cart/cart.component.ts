57d82bf8c9fac0b2ef39bad7bf6c82dddedc9cdf
ptums <ptumulty923@gmail.com>
2023-05-09 21:47:26 -0400

    workign on cart calculations

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
-import { Cart } from 'src/app/types/cart.interface';
+import { Cart, CartItem } from 'src/app/types/cart.interface';
 
 @Component({
   selector: 'app-cart',
@@ -11,10 +11,38 @@ export class CartComponent {
   cart: Cart = {
     items: [],
   };
+  total: string;
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService) {
+    this.total = '0';
+  }
 
   ngOnInit() {
     this.cart.items = this.cartService.getCartItems();
+    this.total = this.setCartTotal(this.cart.items);
+  }
+
+  ngOnChanges() {
+    console.log('test..');
+  }
+
+  setCartTotal(items: CartItem[]): string {
+    let results = 0;
+
+    items.forEach((item: CartItem) => {
+      results = results += item.product.price * item.quantity;
+    });
+
+    return results.toFixed(2);
+  }
+
+  increaseQuantity(event: MouseEvent, item: CartItem): void {
+    const quantity = (event?.target as HTMLInputElement).getAttribute(
+      'ng-reflect-model'
+    );
+    console.log({
+      quantity,
+      item,
+    });
   }
 }
